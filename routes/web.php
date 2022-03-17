ae650faa1b9f058a07cdf17e70fa68a3c1679b51
Sávio Gomes da Silva <saviogomesdasilva@outlook.com.br>
2022-03-16 22:02:06 -0300

    configurando telas do sistema

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['middleware' => 'web'], function () {
+    Route::get('/', [App\Http\Controllers\HomeController::class, 'index']);
 });
+
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Auth::routes();
 
